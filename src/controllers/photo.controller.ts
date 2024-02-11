94c14feecc690fe76842d768fdffe77c3ee27d1c
johan101429 <jona1014.mr@gmail.com>
2024-02-11 17:29:51 -0500

    se agregan nuevas imagenes

--- src/controllers/photo.controller.ts
+++ src/controllers/photo.controller.ts
@@ -6,14 +6,19 @@ export function getPhotos(req: Request, res: Response) {}
 export async function createPhoto(req: Request, res: Response) {
   const { title, description } = req.body;
   console.log(req.file?.path);
+console.log(req.body);
 
   const newPhoto = {
     title: title,
     description: description,
     imagePath: req.file?.path,
+    
+    
   };
   const photos = new photo(newPhoto);
   await photos.save();
+  
+  
 
   return res.json({
     message: "Photo created successfully",
